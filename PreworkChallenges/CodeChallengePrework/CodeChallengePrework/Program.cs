ff753f44c9206df789a3a39e3f67e79acda69adc
Zscoob <james.zobian@gmail.com>
2020-05-03 18:54:10 -0500

    array max result challenge

--- PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
+++ PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
@@ -6,7 +6,46 @@ namespace CodeChallengePrework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayMaxResult();
         }
+        private static void ArrayMaxResult()
+        {
+            int[] newArray = new int[5];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine("Pick a number between 1 and 10");
+
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < 1 || userInput > 10)
+                {
+                    Console.WriteLine("Number not within limits");
+                    i--;
+                }
+                else
+                {
+                    newArray[i] = userInput;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", newArray));
+
+            Console.WriteLine("Choose one of your numbers");
+
+            int userChoice = int.Parse(Console.ReadLine());
+            int counter = 0;
+            foreach (int number in newArray)
+            {
+                if (userChoice == number)
+                {
+                    counter++;
+                }
+            }
+            int score = userChoice * counter;
+
+            Console.WriteLine($"Score: {score}");
+          
+        }
+
     }
 }
