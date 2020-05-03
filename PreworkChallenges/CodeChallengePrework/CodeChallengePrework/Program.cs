271530c766a186906edc3b32890f27b2d41ac99d
Zscoob <james.zobian@gmail.com>
2020-05-03 17:00:33 -0500

    Local repo created

---
+++ PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallengePrework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
