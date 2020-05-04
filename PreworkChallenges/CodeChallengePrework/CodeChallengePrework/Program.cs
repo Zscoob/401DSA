9de6543ebaeb2e937a0a2e07d80b912ec32916cc
Zscoob <james.zobian@gmail.com>
2020-05-03 19:47:45 -0500

    read me and pics updated

--- PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
+++ PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
@@ -9,8 +9,8 @@ namespace CodeChallengePrework
             LeapYearCalc();
         }
         private static void LeapYearCalc()
-            {
-           
+        {
+
             Console.Write("Input an year : ");
             int year = int.Parse(Console.ReadLine());
 
@@ -24,50 +24,8 @@ namespace CodeChallengePrework
                 Console.WriteLine("{0} is not a leap year.", year);
         }
     }
-
-
-    /*
-    static void Main(string[] args)
-    {
-       ArrayMaxResult();
-    }
-    private static void ArrayMaxResult()
-    {
-        int[] newArray = new int[5];
-        for (int i = 0; i < newArray.Length; i++)
-        {
-            Console.WriteLine("Pick a number between 1 and 10");
-
-            int userInput = int.Parse(Console.ReadLine());
-
-            if (userInput < 1 || userInput > 10)
-            {
-                Console.WriteLine("Number not within limits");
-                i--;
-            }
-            else
-            {
-                newArray[i] = userInput;
-            }
-        }
-
-        Console.WriteLine(string.Join(", ", newArray));
-
-        Console.WriteLine("Choose one of your numbers");
-
-        int userChoice = int.Parse(Console.ReadLine());
-        int counter = 0;
-        foreach (int number in newArray)
-        {
-            if (userChoice == number)
-            {
-                counter++;
-            }
-        }
-        int score = userChoice * counter;
-
-        Console.WriteLine($"Score: {score}");
-    */
 }
+
+ 
 
     
