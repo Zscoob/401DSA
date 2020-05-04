994dc61d76ea466cf34822fa2bde4254c46a6bcb
Zscoob <james.zobian@gmail.com>
2020-05-03 23:39:17 -0500

    sum of rows

--- PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
+++ PreworkChallenges/CodeChallengePrework/CodeChallengePrework/Program.cs
@@ -4,43 +4,55 @@ namespace CodeChallengePrework
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            PerfectSequence();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("***********SUM OF THE ROWS*************");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            TheMatrix();
+            
         }
-        public static void PerfectSequence()
+        private static void TheMatrix()
         {
+            Console.WriteLine("How many rows?");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many columns?");
+            int columns = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n()_()");
+            Console.WriteLine("(*_*)");
+            Console.WriteLine("(\")(\")\n");
 
-            Console.WriteLine("Provide numbers for your array");
-            int arrLength = int.Parse(Console.ReadLine());
-            int inputSum = 0;
-            int inputProduct = 1;
-            int[] userInput = new int[arrLength];
-            for (int i = 0; i < arrLength; i++)
+            int[,] neo = new int[rows, columns];
+            System.Random random = new System.Random();
+
+            for (int i = 0; i < rows; i++)
             {
-                Console.WriteLine("Please enter another number");
-                userInput[i] = int.Parse(Console.ReadLine());
+                for (int j = 0; j < columns; j++)
+                {
+                    int filler = random.Next(3, 45);
+                    neo[i, j] = filler;
+                }
             }
 
-            foreach (int number in userInput)
-            {               
-                inputProduct *= number;
-                inputSum += number;
-            }
-       
-            if (inputSum > 0 && inputSum == inputProduct)
+            int[] output = new int[rows];
+            int totals = 0;
+
+            for (int m = 0; m < neo.GetLength(0); m++)
             {
-                Console.WriteLine("Yes");
+                for (int n = 0; n < neo.GetLength(1); n++)
+                {
+                    totals += neo[m, n];
+                    Console.Write(string.Format("{0} ", neo[m, n]));
+                }
+                Console.Write("\n\n");
+                output[m] = totals;
             }
-            else
-            {
-                Console.WriteLine("No");
-            }
+            Console.Write("Totals: ");
+            Console.WriteLine(string.Join(",", output));
         }
+
     }
 }
 
 
- 
 
-    
--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # 401DSA
 401 .NET Core, React DSA
-# Problem 3: Perfect Sequence
+# Problem 4: Sum of Rows
 Problem Statement
-Given an array, return “Yes” if the sequence is considered a perfect sequence. Otherwise, return “No”
-
-Perfect Sequences
-A perfect sequence is a sequence such that all of its elements are non-negative integers and the product of all of them is equal to their sum. For example: [2,2], [1,3,2] and [0,0,0,0] are perfect sequences and [4,5,6] and [0,2,-2] are not perfect sequences. Negative numbers of any kind are not valid in a perfect sequence
+Given a matrix of integers. Return the sum of each row in a single dimensional array.
 
 Input Format
-An array of integers (may not be sorted)
+a multidimensional array with the dimensions of m x n (m = rows, n = columns).
+
+duplicate integers are possible.
+Negative numbers are possible
+both m and n can vary in length
+The user should specify the length and the width of the array within the console. To populate the numbers, you may randomly generate them, or have the user input one by one.
+
+The method should take in the multidimensional array and return the single dimensional array with the sums
 
 Sample Input
-[1,3,2]
+int[,] myArray = new int[3, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
+^^ The actual input would just be myArray, but the above code shows the pre-initialization before the method is called.
 
 Sample Output
-Yes
+{15,40,65} <– an integer array
 
-Explanation
-1 * 3 * 2 = 6 && 1 + 3 + 2 = 6
-
+HINTS
+for loops are your best friend
+GetLength() is a method you should look into
+i.e. in the array above myArray.GetLength(0) is equal to 3
+myArray.GetLength(1) is equal to 5
 Readme
 A section of your readme should contain basic information outlining the requirements for the challenge
 
